0e94d2adec582f330919e863ab4f906005fe1412
Shuhong Lu <uklawson@gmail.com>
2024-08-22 16:05:32 +0100

    initialise Spring boot application

---
+++ src/test/java/com/starlingbank/roundUpSaving/RoundUpSavingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlingbank.roundUpSaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundUpSavingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
