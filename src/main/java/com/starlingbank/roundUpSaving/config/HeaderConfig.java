0a05571cba721db62c67100509d02e96dd94ef51
Shuhong Lu <uklawson@gmail.com>
2024-08-23 12:03:13 +0100

    configure header and RestTemplate to be used in services

---
+++ src/main/java/com/starlingbank/roundUpSaving/config/HeaderConfig.java
@@ -0,0 +1,24 @@
+package com.starlingbank.roundUpSaving.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+@Configuration
+public class HeaderConfig {
+
+    @Value("${customer.token.access}")
+    private String accessToken;
+
+    public HttpHeaders constructHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.add("Authorization", accessToken);
+
+        return httpHeaders;
+    }
+}
---
+++ src/main/java/com/starlingbank/roundUpSaving/services/SavingGoalService.java
@@ -0,0 +1,4 @@
+package com.starlingbank.roundUpSaving.services;
+
+public class SavingGoalService {
+}
